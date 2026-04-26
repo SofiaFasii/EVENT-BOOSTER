49cf9c935cc734af0331c5788f8f69733371cc5d
SofiaFasii <sofiafasii97@gmail.com>
2026-04-26 22:02:04 +0300

    l

--- src/js/api.js
+++ src/js/api.js
@@ -1,7 +1,6 @@
 const BASE_URL = "https://app.ticketmaster.com/discovery/v2/events.json"
 const API_KEY = "tG7aQ3GCK35UI1b0FVQGBmQ2kSZJ2t4e"
 
-fetch(`${BASE_URL}?apikey=${API_KEY}`)
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
+export async function searchEvents(keyword){
+    
+}
