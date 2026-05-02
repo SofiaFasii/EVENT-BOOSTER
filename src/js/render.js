dea87ee7cf2fd0b019c209d7296a3ba3626c0179
SofiaFasii <sofiafasii97@gmail.com>
2026-05-02 22:46:23 +0300

    -sass

--- src/js/render.js
+++ src/js/render.js
@@ -3,14 +3,17 @@ const list = document.querySelector(".events-list");
 export function renderEvents(events) {
   const markup = events
     .map((event) => {
+
       return `
-            <li class="event" data-id="${event.id}">
-                <img src="${event.images[0].url} class="event-img" alt="">
-                <h3>${event.name}</h3>
-                <p>${event.dates.start.localDate}</p>
-            </li>
-        `;
+        <li class="event" data-id="${event.id}">
+          <img src="${event.images[0].url}" alt="${event.name}" class="event-img">
+          <h3>${event.name}</h3>
+          <p>${event.dates.start.localDate}</p>
+          <p>${event._embedded.venues[0].name}</p>
+        </li>
+      `;
     })
+    .join("")
 
   list.innerHTML = markup;
 }
