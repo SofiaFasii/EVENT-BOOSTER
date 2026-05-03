7bb2cec1bb07ca1c15cca7c783dd25a949b67047
SofiaFasii <sofiafasii97@gmail.com>
2026-05-03 21:55:31 +0300

    q

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,15 @@
 import { fetchEvents } from "./api";
 import { renderEvents } from "./render";
+import { countries } from "./countries";
 
-let country = "US"
-let keyword = "events"
-let page = 1
+let country = ""
+let keyword = ""
+let page = 0
+
+const searchInput = document.getElementById('searchInput')
+const searchBtn = document.getElementById('searchBtn')
+const countryInput = document.getElementById('countryInput')
+
 
 async function init() {
   const data = await fetchEvents({country, keyword, page});
